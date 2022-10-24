942c88ca500d4ec5950b260fd2a0facf6929fc16
Mark <mark.magrisy@yahoo.com>
2022-10-24 23:18:18 +0200

    project final 1

--- README.md
+++ README.md
@@ -11,3 +11,15 @@ The starter project has some HTML and CSS styling to display a static version of
 To get started, open `js/app.js` and start building out the app's functionality
 
 For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+
+
+##The Page:
+This is a Landing Page used in websites by marketeers to attract clients and make leads.
+**The app.Js** has the functions to:
+-Build the navigation bar that is updated when new sections are added to the page.
+-Set the section in the viewport as active.
+-Smooth scrolling from the navigation bar to the rest of the page.
+-A to top button which appears when scrolling down 400px.
+
+The page has also responsive design.
--- js/app.js
+++ js/app.js
@@ -18,45 +18,85 @@
  * Great to have comments before crucial code sections within the procedure.
 */
 
-/**
- * Define Global Variables
- * 
+/*
+ Define Global Variables
+*/
+
+//storing the nav ul  in a const
+const navList = document.querySelector("#navbar__list");
+
+//storing the creation of li in a const
+const newListItem = document.createElement("li");
+
+//storing the sections and their nav-data in variables
+let sections = document.querySelectorAll("section");
+
+let navData = [];
+for (let i = 0; i < sections.length; i++) {
+    navData.push(sections[i].getAttribute("data-nav"));
+}
+
+//getting the sections IDs
+let sectionsID = [];
+for (let i = 0; i < sections.length; i++) {
+    sectionsID.push(sections[i].getAttribute("id"));
+}
+
+//To top button
+let myButton = document.getElementById("totop");
+
+
+
+/*
+ End Global Variables
 */
 
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
 
 // build the nav
+for(let i = 0; i < sections.length; i++) {
+    newListItem.innerHTML = `<a href="#${sectionsID[i]}">${navData[i]}</a>` 
+    navList.appendChild(newListItem.cloneNode(true)).setAttribute('class', 'menu__link'); //cloneNode avoids unwanted closures and lets the loop create 4 distinct nodes instead of creating the loop 4 times 
+}
 
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
 
 // Scroll to section on link click
+let items = navList.children;
+navList.addEventListener('click', (e) => {
+        e.preventDefault();
+        let item = e.target.closest('li');
+        let order = Array.from(items).indexOf(item);
+        document.querySelector(`#section${order + 1}`).scrollIntoView({behavior: 'smooth'});
+    });
+
 
 // Set sections as active
+window.onscroll = function() {
+    sections.forEach( (section) => {
+        if (section.getBoundingClientRect().y <= 250 && section.getBoundingClientRect().y >= -350) {
+            section.classList.add('your-active-class');
+        } else {
+            section.classList.remove('your-active-class');
+        }
+    });
+
+//To top button display:
+    if (window.scrollY >= 400) {
+        myButton.classList.remove("hidden");
+    } else {
+        myButton.classList.add("hidden");
+    }
+}
+
+//to top function
+myButton.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+    
+}
+
 
 
